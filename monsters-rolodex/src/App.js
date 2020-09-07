08db974a9fc5e04e7c7c7d746bd5724dd1ddb33a
Amogh Pradhan <amopradhan@gmail.com>
2020-09-07 17:03:42 -0400

    full monsters

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
       monsters: [],
+      date: new Date(),
+      searchField: "",
     };
   }
 
   componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
 
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state; // equivalent to const monsters = this.state.monsters , const searchField = this.state.searchField
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <h1>It is {this.state.date.toLocaleTimeString()}</h1>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }

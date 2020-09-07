24ec1afc0dceb26a5b40829247839733b4b4fe5d
Amogh Pradhan <amopradhan@gmail.com>
2020-09-07 17:42:48 -0400

    adding all files for Github

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,50 +1,41 @@
-import React, { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component";
-import { SearchBox } from "./components/search-box/search-box.component";
-import "./App.css";
+import React, { Component } from 'react';
 
-class App extends React.Component {
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+import './App.css';
+
+class App extends Component {
   constructor() {
     super();
 
     this.state = {
       monsters: [],
-      date: new Date(),
-      searchField: "",
+      searchField: ''
     };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
-
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
   };
 
   render() {
-    const { monsters, searchField } = this.state; // equivalent to const monsters = this.state.monsters , const searchField = this.state.searchField
-    const filteredMonsters = monsters.filter((monster) =>
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
-      <div className="App">
-        <h1>It is {this.state.date.toLocaleTimeString()}</h1>
+      <div className='App'>
         <h1>Monsters Rolodex</h1>
-        <SearchBox
-          placeholder="search monsters"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters}></CardList>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import "./card.styles.css";
+import './card.styles.css';
 
-export const Card = (props) => (
-  <div className="card-container">
+export const Card = props => (
+  <div className='card-container'>
     <img
-      alt="monster"
+      alt='monster'
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
     <h2> {props.monster.name} </h2>
-    <h2> {props.monster.email} </h2>
+    <p> {props.monster.email} </p>
   </div>
 );
